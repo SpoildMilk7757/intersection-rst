3f2d0f3ca1d1d0adb15786257e91a474955f8b6d
SpoildMilk7757 <101339945+SpoildMilk7757@users.noreply.github.com>
2022-05-17 09:53:55 -0400

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -25,18 +25,14 @@ radio.onReceivedNumber(function (receivedNumber) {
         Ambulance()
     }
 })
-input.onButtonPressed(Button.A, function () {
-    if (xyz == 1) {
-        xyz = 0
-        Pedestrian_Crossing()
-        basic.pause(3000)
-        GREEN()
-        basic.pause(15000)
-        YELLOW()
-        basic.pause(4000)
-        RED()
-    }
-})
+function Sonar () {
+    pins.digitalWritePin(DigitalPin.P1, 0)
+    control.waitMicros(2)
+    pins.digitalWritePin(DigitalPin.P1, 1)
+    control.waitMicros(10)
+    pins.digitalWritePin(DigitalPin.P1, 0)
+    Distance = pins.pulseIn(DigitalPin.P2, PulseValue.High) / 58
+}
 function RED () {
     range = Strip.range(0, 1)
     range.showColor(neopixel.colors(NeoPixelColors.Red))
@@ -59,7 +55,6 @@ function Ambulance () {
     YELLOW()
     basic.pause(4000)
     RED()
-    radio.sendNumber(8)
 }
 function Vehicle () {
     basic.showIcon(IconNames.No)
@@ -77,21 +72,6 @@ function GREEN () {
     range = Strip.range(2, 1)
     range.showColor(neopixel.colors(NeoPixelColors.Green))
 }
-input.onButtonPressed(Button.B, function () {
-    if (xyz == 1) {
-        xyz = 0
-        music.playTone(587, music.beat(BeatFraction.Quarter))
-        music.playTone(698, music.beat(BeatFraction.Half))
-        Pedestrian_Crossing_VI()
-        basic.pause(3000)
-        GREEN()
-        basic.pause(15000)
-        YELLOW()
-        basic.pause(4000)
-        RED()
-        xyz = 1
-    }
-})
 function Pedestrian_Crossing_VI () {
     basic.showLeds(`
         . . # . .
@@ -125,30 +105,42 @@ function YELLOW () {
     range = Strip.range(2, 1)
     range.showColor(neopixel.colors(NeoPixelColors.Black))
 }
-let Distance = 0
 let range: neopixel.Strip = null
+let Distance = 0
 let Strip: neopixel.Strip = null
 let Pedestrian_Crossing_Time = 0
-let xyz = 0
-xyz = 1
+let xyz = 1
 Pedestrian_Crossing_Time = 15
-Strip = neopixel.create(DigitalPin.P1, 3, NeoPixelMode.RGB)
+Strip = neopixel.create(DigitalPin.P16, 3, NeoPixelMode.RGB)
 Strip.setBrightness(20)
 radio.setGroup(177)
 basic.showIcon(IconNames.No)
 RED()
 basic.forever(function () {
-    pins.digitalWritePin(DigitalPin.P1, 0)
-    control.waitMicros(2)
-    pins.digitalWritePin(DigitalPin.P1, 1)
-    control.waitMicros(10)
-    pins.digitalWritePin(DigitalPin.P1, 0)
-    Distance = pins.pulseIn(DigitalPin.P2, PulseValue.High) / 58
+    for (let index = 0; index < 10; index++) {
+        Sonar()
+    }
     if (Distance > 4 && Distance < 6) {
         Vehicle()
     }
     if (input.buttonIsPressed(Button.A)) {
-        basic.pause(48000)
-        xyz = 1
+        Pedestrian_Crossing()
+        basic.pause(3000)
+        GREEN()
+        basic.pause(15000)
+        YELLOW()
+        basic.pause(4000)
+        RED()
+    }
+    if (input.buttonIsPressed(Button.B)) {
+        music.playTone(587, music.beat(BeatFraction.Quarter))
+        music.playTone(698, music.beat(BeatFraction.Half))
+        Pedestrian_Crossing_VI()
+        basic.pause(3000)
+        GREEN()
+        basic.pause(15000)
+        YELLOW()
+        basic.pause(4000)
+        RED()
     }
 })
